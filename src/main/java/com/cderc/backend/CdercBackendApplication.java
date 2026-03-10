218f8aa8bfa6b05ba569274266b16df9b035660a
Your Name <akofa.djomatin@gmail.com>
2026-03-10 13:39:57 +0100

    Initial commit: Sprint Boot project skeleton

---
+++ src/main/java/com/cderc/backend/CdercBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cderc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdercBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdercBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cderc/backend/CdercBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cderc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdercBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
