e1f3e0412737cfc0df88999ca9c4f01dd4fb42b2
Your Name <akofa.djomatin@gmail.com>
2026-03-18 12:54:35 +0100

    Fi env variable fr datasource

--- src/main/java/com/cderc/backend/config/SecurityConfig.java
+++ src/main/java/com/cderc/backend/config/SecurityConfig.java
@@ -81,12 +81,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    @Primary
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors().and()
